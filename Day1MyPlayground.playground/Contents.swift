9eddf31ae97d7550b0620f83f0db4f5d241393f2
HONG JEONGSEOB <jeongseob.hong@progrit.co.jp>
2023-09-25 10:29:19 +0900

    feat: Add Day1!

---
+++ Day1MyPlayground.playground/Contents.swift
@@ -0,0 +1,50 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let result = "❤️ love ❤️"
+movie.count
+result.count
+
+movie.hasPrefix("A day")
+
+
+let reallyBig = 1_000_0000000______00
+let reallyBig2 = 1000000000000
+
+var counter = 10
+
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+print(c)
+
+let double1 = 3.1
+let double2 = 313131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name2 = "Nicolas Cage"
+name = "John Trabolta"
